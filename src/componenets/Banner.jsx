dea7c8d7da6616e106b567d97831ce9667059a1c
pritam dutta <webdevpritam7498@gmail.com>
2024-09-01 18:40:48 +0600

    banner is completed

--- src/componenets/Banner.jsx
+++ src/componenets/Banner.jsx
@@ -1,14 +1,116 @@
-import React from 'react'
+import React from "react";
+import banner from "../assets/home/banner.png";
+import bannerDesignImg1 from "../assets/home/b-food1.png";
 
 const Banner = () => {
   return (
-    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 bg-gradient-to-r from-[#fafafa] from-0% to-[#fcfcfc] to-100% '>
-      <div className="py-24 flex flex-col md:flex-row justify-between items-center gap-8">
-        {/* left-text */}
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 bg-gradient-to-r from-[#fafafa] from-0% to-[#fcfcfc] to-100% ">
+      <div className="py-24 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
         {/* right-img */}
+        <div className="md:w-1/2">
+          <img src={banner} alt="banner" />
+          <div className="flex flex-col md:flex-row items-center justify-around -mt-14 ml-10">
+            {/* banner img-2 */}
+            <div className="flex bg-white rounded-2xl items-center gap-3 shadow-md">
+              
+              <div className="space-y-1 px-5">
+                <h5>Spicy noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+                <p className="text-red font-semibold">$20.00</p>
+              </div>
+              <img
+                src={bannerDesignImg1}
+                alt="banner design img"
+                className="rouned-2xl"
+              />
+            </div>
+            {/* banner img-3 */}
+            <div className="sm:flex hidden bg-white rounded-2xl items-center gap-3 shadow-md">
+              <img
+                src={bannerDesignImg1}
+                alt="banner design img"
+                className="rouned-2xl"
+              />
+              <div className="space-y-1 px-5">
+                <h5>Spicy noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+                <p className="text-red font-semibold">$20.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* left-text */}
+        <div className="md:w-1/2 space-y-7 px-4">
+          <h2 className="md:text-5xl text-4xl font-bold leading-snug md:leading-snug">
+            Lorem ipsum dolor sit amet. <span className="text-green">Food</span>{" "}
+          </h2>
+          <p className="text-xl text-[#4a4a4a]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, et!
+          </p>
+          <button className="btn bg-green px-8 py-3 font-semibold text-white rounded-full">
+            Order Now
+          </button>
+        </div>
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
