91515a659500b9607d25e625daccfc8092f174b3
pritam dutta <webdevpritam7498@gmail.com>
2024-09-03 19:28:03 +0600

    footer is ready

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../componenets/Navbar'
+import Footer from '../componenets/Footer'
 
 
 const Main = () => {
@@ -8,6 +9,7 @@ const Main = () => {
     <div>
         <Navbar/>
         <Outlet/>
+        <Footer/>
         
     </div>
   )
