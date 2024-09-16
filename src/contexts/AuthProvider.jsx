9d42d7540f694735cd74422cedf26b3c5c7b6139
pritam dutta <webdevpritam7498@gmail.com>
2024-09-16 10:09:09 +0600

    basic authProvider is done

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,11 +1,74 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
+const auth = getAuth();
+const googleProvider = new GoogleAuthProvider();
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  // crate account
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // signup with gmail
+  const signInWithGmail = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //login using email and pass
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //logout
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  //update profile
+  const updateUserProfile = ({ name, photoURL }) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photoURL,
+    });
+  };
+
+  // check singed in user
+  useEffect(()=>{
+  const unsubscribe =  onAuthStateChanged(auth, (currentUser) => {
+      if(currentUser){
+        setUser(currentUser);
+        setLoading(false);
+      }
+      else{
+
+      }
+    })
+    return () =>{
+      return unsubscribe();
+    }
+  }, [])
   const authInfo = {
     user,
+    createUser,
+    signInWithGmail,
+    login,
+    logOut,
+    updateUserProfile,
+    
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
